4d4b4723bd4d1487440cf2adab1a427ac8aca9cc
Rohit1144 <rohitchowdary1144@gmail.com>
2023-05-24 15:09:22 +0530

    changes done

--- input how.py
+++
@@ -1,3 +0,0 @@
-n = input('enter number')
-print(n)
-print(n + " is a number")
---
+++ input.py
@@ -0,0 +1,7 @@
+n = input('enter number')
+num = int(n)
+
+if num > 10:
+    print('uno madre')
+else:
+    print('nein')
